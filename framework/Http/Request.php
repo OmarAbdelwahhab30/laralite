58a92cf522ebddb1de6fb4c49aef3e51af3251ab
omarkishk <omarabdelwahhabkishk2000@gmail.com>
2023-10-21 01:29:01 +0200

    first commit

---
+++ framework/Http/Request.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace laralite\framework\Http;
+
+class Request
+{
+
+
+    public function __construct(
+        public readonly array $getParams,
+        public readonly array $postParams,
+        public readonly array $cookies,
+        public readonly array $files,
+        public readonly array $server
+    )
+    {
+    }
+
+    public static function createFromGlobals(): static
+    {
+        return new static($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
+    }
+
+}
