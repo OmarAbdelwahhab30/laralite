87c94d5a1d7faa0ff2699130b3d2113efd41c2a5
omarkishk <omarabdelwahhabkishk2000@gmail.com>
2023-11-05 22:48:57 +0200

    addin files

--- framework/src/Container/Container.php
+++ framework/src/Container/Container.php
@@ -3,6 +3,7 @@
 namespace Laralite\Framework\Container;
 
 use Psr\Container\ContainerInterface;
+use ReflectionException;
 
 class Container implements ContainerInterface
 {
@@ -22,9 +23,64 @@ class Container implements ContainerInterface
         $this->services[$id] = $concrete;
     }
 
+    /**
+     * @throws ReflectionException
+     * @throws ContainerException
+     */
     public function get(string $id)
     {
-        return new $this->services[$id];
+        if (!$this->has($id)) {
+            if (!class_exists($id)) {
+                throw new ContainerException("Service $id cannot be resolved");
+            }
+            $this->add($id);
+        }
+        return $this->resolve($this->services[$id]);
+    }
+
+    /**
+     * @throws ReflectionException|ContainerException
+     */
+    private function resolve($class)
+    {
+        $reflection = new \ReflectionClass($class);
+
+        $constructor = $reflection->getConstructor();
+
+        if (null === $constructor) {
+
+            return $reflection->newInstance();
+
+        }
+
+        $constructorArgs = $constructor->getParameters();
+
+        $dependencies = $this->resolveConstructorArgs($constructorArgs);
+
+        return $reflection->newInstanceArgs($dependencies);
+    }
+
+    /**
+     * @throws ReflectionException
+     * @throws ContainerException
+     */
+    private function resolveConstructorArgs(array $constructorArgs): array
+    {
+        $dependencies = [];
+
+        foreach ($constructorArgs as $arg) {
+
+            $classType = $arg->getType();
+
+            $className = $classType->getName();
+
+            $dependency = $this->get($className);
+
+            $dependencies[] = $dependency;
+
+        }
+
+        return $dependencies;
     }
 
     public function has(string $id): bool
--- framework/tests/DependantClass.php
+++ framework/tests/DependantClass.php
@@ -5,4 +5,14 @@ namespace Laralite\Framework\Tests;
 class DependantClass
 {
 
+    public DependencyClass $dependency;
+    public function __construct(DependencyClass $dependency,int $id)
+    {
+        $this->dependency = $dependency;
+    }
+
+    public function getDependency(): DependencyClass
+    {
+        return $this->dependency;
+    }
 }
