b1a68d9500c964abb5cce8e82eb6df8167b12a46
omarkishk <omarabdelwahhabkishk2000@gmail.com>
2023-11-07 20:43:02 +0200

    adding files

--- framework/src/Http/Kernel.php
+++ framework/src/Http/Kernel.php
@@ -3,33 +3,53 @@
 namespace Laralite\Framework\Http;
 
 use Laralite\Framework\Router\RouterInterface;
+use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\ContainerInterface;
+use Psr\Container\NotFoundExceptionInterface;
 
 class Kernel
 {
 
     public RouterInterface $router;
     public ContainerInterface $container;
-    public function __construct(RouterInterface $router,ContainerInterface $container)
+    private $appEnv;
+
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    public function __construct(RouterInterface $router, ContainerInterface $container)
     {
+
+
         $this->router = $router;
         $this->container = $container;
+        $this->appEnv = $this->container->get("APP_ENV");
     }
 
     public function handle(Request $request)
     {
+
         try {
-            [$routeHandler, $vars] = $this->router->dispatch($request,$this->container);
+            [$routeHandler, $vars] = $this->router->dispatch($request, $this->container);
             $response = call_user_func_array($routeHandler, $vars);
-
-
-        } catch (HttpRequestMethodException $exception) {
-            $response = new Response($exception->getMessage(), $exception->getStatusCode());
-        } catch (HttpException $exception) {
-            $response = new Response($exception->getMessage(), $exception->getStatusCode());
         } catch (\Exception $exception) {
-            $response = new Response($exception->getMessage(), 500);
+            $response = $this->createResponseException($exception);
         }
         return $response;
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function createResponseException(\Exception $exception): Response
+    {
+        if (in_array($this->appEnv, ["dev", "test"])) {
+            throw $exception;
+        }
+        if ($exception instanceof HttpException) {
+            return new Response($exception->getMessage(), $exception->getStatusCode());
+        }
+        return new Response("Internal Server Error", Response::HTTP_INTERNAL_SERVER_ERROR);
     }
 }
