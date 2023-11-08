11c7fc6983134792afe6256684a86876093153db
omarkishk <omarabdelwahhabkishk2000@gmail.com>
2023-11-08 23:53:06 +0200

    adding files

--- config/services.php
+++ config/services.php
@@ -1,25 +1,52 @@
 <?php
 
+use Laralite\Framework\Controllers\AbstractController;
 use Laralite\Framework\Http\Kernel;
 use Laralite\Framework\Router\Router;
 use Laralite\Framework\Router\RouterInterface;
 use League\Container\Argument\Literal\ArrayArgument;
+use League\Container\Argument\Literal\StringArgument;
 use League\Container\Container;
+use League\Container\ReflectionContainer;
+use Symfony\Component\Dotenv\Dotenv;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
-$routes = include BASE_PATH.DIRECTORY_SEPARATOR.'routes'.DIRECTORY_SEPARATOR.'web.php';
+$routes = include BASE_PATH . DIRECTORY_SEPARATOR . 'routes' . DIRECTORY_SEPARATOR . 'web.php';
+
+$dotEnv = new Dotenv();
+
+$dotEnv->load(dirname(__DIR__) . DIRECTORY_SEPARATOR . ".env");
+
+$APP_ENV = $_SERVER['APP_ENV'];
+
+$templatesPath = BASE_PATH . DIRECTORY_SEPARATOR . 'templates';
 
 $container = new Container();
 
-$container->delegate(new \League\Container\ReflectionContainer(true));
+$container->delegate(new ReflectionContainer(true));
+
+$container->add("APP_ENV", new StringArgument($APP_ENV));
 
 $container->add(RouterInterface::class,
     Router::class);
 
 $container->extend(RouterInterface::class)
-    ->addMethodCall("setRoutes",[new ArrayArgument($routes)]);
+    ->addMethodCall("setRoutes", [new ArrayArgument($routes)]);
 
 $container->add(Kernel::class)
     ->addArgument(RouterInterface::class)
     ->addArgument($container);
+
+$container->addShared('filesystem-loader', FilesystemLoader::class)
+    ->addArgument(new StringArgument($templatesPath));
+
+$container->addShared(Environment::class)
+    ->addArgument('filesystem-loader');
+
+$container->add(AbstractController::class);
+
+$container->inflector(AbstractController::class)
+    ->invokeMethod('setContainer', [$container]);
 
 return $container;
--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,6 @@ return [
     ["GET",'/user/{name:.+}',function($name){
         return new \Laralite\Framework\Http\Response("My name is ".$name);
     }],
+
+    ["GET","/post/{id}",[\App\controllers\HomeController::class,"index"]],
 ];
